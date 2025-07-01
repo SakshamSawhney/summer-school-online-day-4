bf3933b09f361941226e97cc1093b01fe202a03d
Saksham Sawhney <123295305+SakshamSawhney@users.noreply.github.com>
2025-07-01 16:44:14 +0530

    Create functions.js

---
+++ functions.js
@@ -0,0 +1,17 @@
+// Declaration
+function add(a, b) {
+  return a + b;
+}
+
+// Expression
+const square = function(n) {
+  return n * n;
+};
+
+// Arrow Function
+const greet = (name) => `Hello, ${name}!`;
+
+// Calling and Logging
+console.log("Sum:", add(5, 7));
+console.log("Square:", square(4));
+console.log(greet("Saksham"));
