fa314f3e21f8c60b1ad8ccfd2b64ddfba647003e
Saksham Sawhney <123295305+SakshamSawhney@users.noreply.github.com>
2025-07-01 16:45:23 +0530

    Create loopingNumbers.js

---
+++ loopingNumbers.js
@@ -0,0 +1,18 @@
+// for loop: 1 to 10
+for (let i = 1; i <= 10; i++) {
+  console.log("for:", i);
+}
+
+// while loop: even numbers from 2 to 20
+let even = 2;
+while (even <= 20) {
+  console.log("while:", even);
+  even += 2;
+}
+
+// do...while loop: 10 to 1
+let count = 10;
+do {
+  console.log("do...while:", count);
+  count--;
+} while (count >= 1);
