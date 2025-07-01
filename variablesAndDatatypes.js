c95a01a8022e12a4613f54b998eeaac5b2cfbc1e
Saksham Sawhney <123295305+SakshamSawhney@users.noreply.github.com>
2025-07-01 16:46:01 +0530

    Create variablesAndDatatypes.js

---
+++ variablesAndDatatypes.js
@@ -0,0 +1,11 @@
+let name = "Saksham";           // String
+const age = 21;                 // Number
+var isStudent = true;          // Boolean
+let country;                   // Undefined
+let marks = null;              // Null
+
+console.log(typeof name);      // string
+console.log(typeof age);       // number
+console.log(typeof isStudent); // boolean
+console.log(typeof country);   // undefined
+console.log(typeof marks);     // object (this is a known JS quirk!)
